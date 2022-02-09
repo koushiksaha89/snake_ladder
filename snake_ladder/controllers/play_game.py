e7cc9375d258aa3ac6a11daeb1c43d572600338a
Koushik Saha <koushik.saha@redbus.com>
2022-02-09 17:33:35 +0000

    updated play game

--- snake_ladder/controllers/play_game.py
+++ snake_ladder/controllers/play_game.py
@@ -11,9 +11,13 @@ class GameController(Board):
 
     def play_game(self, player: Player, turn_num):
         # assumption: both ladder and snake does not exists at the same location
-        # print(f'player_id {player.player_id} turn_no {turn_num}')
+        print(f'player_id {player.player_id} turn_no {turn_num}')
+        climb_turn_dict = {turn_num : list()}
+        slide_turn_dict = {turn_num : list()}
+        
         while True:
             dice_value = self.roll_the_dice()
+            player.total_number_of_dice_rolls += 1
             tentative_location = confirmed_location = player.pos + dice_value
             if tentative_location <= self.board_end_loc:
                 ladder_loc = self.ladder_map.get(tentative_location)
@@ -21,13 +25,21 @@ class GameController(Board):
 
                 if ladder_loc is not None:
                     amount_of_climb = ladder_loc-tentative_location
-                    player.climb_amount_history.append(amount_of_climb)
+                    climb_turn_dict[turn_num].extend([amount_of_climb])
                     confirmed_location = ladder_loc
+                    player.total_no_of_lucky_rolls +=1
 
                 if snake_loc is not None:
                     amount_of_slide = tentative_location-snake_loc
-                    player.slide_amount_history.append(amount_of_slide)
+                    slide_turn_dict[turn_num].extend([amount_of_slide])
                     confirmed_location = snake_loc
+                    player.total_no_of_unlucky_rolls +=1
+                
+                # Misses a snake by 1 or 2 steps
+                snake_in_next_location = self.snake_map.get(tentative_location+1)
+                snake_in_next_next_location = self.snake_map.get(tentative_location+2)
+                if snake_in_next_location is not None or snake_in_next_next_location is not None:
+                    player.total_no_of_lucky_rolls += 1
 
                 player.pos = confirmed_location
                 player.is_winner = True if confirmed_location == self.board_end_loc else False
@@ -37,4 +49,9 @@ class GameController(Board):
 
             if dice_value < 6 or player.is_winner == True:
                 break
+        
+        if list(climb_turn_dict.values())[0] != []:
+            player.climb_amount_history.append(climb_turn_dict)
+        if list(slide_turn_dict.values())[0] != []:
+            player.slide_amount_history.append(slide_turn_dict)
         return player
