06c555d551457b2ea43852fb8386d45dbd178007
Koushik Saha <koushik.saha@redbus.com>
2022-02-09 08:59:33 +0000

    updated initial values of stats

--- snake_ladder/domain_models/player_stats.py
+++ snake_ladder/domain_models/player_stats.py
@@ -8,11 +8,11 @@ class PlayerStat(ABC):
 
         super().__init__()
 
-        self.total_number_of_dice_rolls = None
-        self.total_climbs = None
-        self.total_slides = None
-        self.biggest_climb_in_a_single_turn = None
-        self.biggest_slide_in_a_single_turn = None
-        self.longest_turn = None
-        self.total_no_of_unlucky_rolls = None
-        self.total_no_of_lucky_rolls = None
+        self.total_number_of_dice_rolls = 0
+        self.total_climbs = 0
+        self.total_slides = 0
+        self.biggest_climb_in_a_single_turn = 0
+        self.biggest_slide_in_a_single_turn = 0
+        self.longest_turn = 0
+        self.total_no_of_unlucky_rolls = 0
+        self.total_no_of_lucky_rolls = 0
