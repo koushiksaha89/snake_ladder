892aeba9465dcc6c73e7b359e5504157451ae1c7
Koushik Saha <koushik.saha@redbus.com>
2022-02-09 15:19:55 +0000

    updated test

--- snake_ladder/tests/test_the_game.py
+++ snake_ladder/tests/test_the_game.py
@@ -1,7 +1,11 @@
 from snake_ladder.entry.initiate_game import GameSetup
 
+# todo : clear the player stat after each round
+simulation_count = 5
+
 g = GameSetup(num_of_snakes=15, num_of_ladders=13)
 g.generate_player_list(num_of_players=5)
-winner_of_the_game = g.start_game(10)
 
-print(f'winner of the game {winner_of_the_game.__dict__}')
+for i in range(0,simulation_count):
+    winner_of_the_game = g.start_game(10)
+    print(f'winner of the game {winner_of_the_game.full_name} for simulation id: {i}')
