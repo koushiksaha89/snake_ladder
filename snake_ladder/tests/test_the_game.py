4d9bfa6e4a485304c123fd23f7a3cabb530e9a09
Koushik Saha <koushik.saha@redbus.com>
2022-02-10 16:38:14 +0000

    updated test_the_game

--- snake_ladder/tests/test_the_game.py
+++ snake_ladder/tests/test_the_game.py
@@ -5,7 +5,6 @@ from rich.console import Console
 
 console = Console()
 
-# todo : clear the player stat after each round
 simulation_count = 5
 
 g = GameSetup(num_of_snakes=18, num_of_ladders=3)
@@ -14,4 +13,4 @@ player_list = g.generate_player_list(num_of_players=3)
 for i in range(0, simulation_count):
     winner_of_the_game = g.start_game(i, deepcopy(player_list))
     console.log(
-        f'winner of the game {winner_of_the_game.full_name} {winner_of_the_game.mobile_number} for simulation id: {i}')
+        f'winner of the game {winner_of_the_game.__dict__} for simulation id: {i}' ,end='\n\n')
